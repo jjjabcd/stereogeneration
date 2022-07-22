95a48136785847d738eb5442d8755582a4461820
Gary Tom <gary.tom@mail.mcgill.ca>
2022-07-21 21:31:39 -0400

    Stereogeneration with JANUS.

---
+++ main.py
@@ -0,0 +1,265 @@
+import os, sys
+import csv
+import time
+import tempfile, inspect
+
+from stereogeneration import JANUS
+from stereogeneration.utils import sanitize_smiles
+from stereogeneration.filter import passes_filter
+
+import selfies as sf
+import selfies
+import numpy as np
+import pandas as pd
+
+import rdkit.Chem as Chem
+from rdkit.Chem import Draw
+from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers
+from morfeus.conformer import ConformerEnsemble
+
+import multiprocessing
+import subprocess
+import re
+
+WORK_DIR = 'cache'
+
+def fitness_function(smi):
+    cwd = os.getcwd()
+    tmp_dir = tempfile.TemporaryDirectory(dir='/tmp')
+    os.chdir(tmp_dir.name)
+
+    path = os.path.join(cwd, 'docking')
+
+    # check the cache
+    if os.path.isfile(os.path.join(path, 'cache.csv')):
+        with open(os.path.join(path, 'cache.csv'), 'r') as f:
+            reader = csv.reader(f)
+            cache = {rows[0]: float(rows[1]) for rows in reader}
+    else:
+        cache = []
+
+    # name = re.sub(r'[^\w]', '', smi)
+    name = 'mol'
+    if smi in cache:
+        score = cache[smi]
+    else:
+        t0 = time.time()
+
+        # do conformer search using morfeus
+        try:
+            ensemble_p = ConformerEnsemble.from_rdkit(smi, optimize="MMFF94", random_seed=25)
+            ensemble_p.prune_rmsd()
+            ensemble_p.sort()   
+            ensemble_p[0:1].write_xyz(f'{name}.xyz')
+            _ = subprocess.run(f'obabel -ixyz {str(name)}.xyz -O {name}.pdb --best', shell=True, capture_output=True)
+        except:
+            # generate ligand files directly from smiles using openbabel
+            print('Default to openbabel embedding...')
+            _ = subprocess.run(f'obabel -:"{smi}" --gen3d -h -O {name}.pdb --best', shell=True, capture_output=True)
+
+        try:
+            # run docking procedure
+            output = subprocess.run(f"{path}/smina.static -r {path}/receptor.pdb -l {name}.pdb --autobox_ligand \
+                {path}/ligand.pdb --autobox_add 5 --exhaustiveness 16 --seed 42",
+                shell=True, capture_output=True)
+
+            if output.returncode != 0:
+                # print('Job failed.')
+                score = -999.0
+            else:
+                # extract result from output
+                found = False
+                for s in output.stdout.decode('utf-8').split():
+                    if found:
+                        # print(f'{s} kcal/mol')
+                        break
+                    if s == '1':
+                        found = True
+
+                score = -float(s)
+        except:
+            score = -1000.0
+        
+        # write to file
+        with open(os.path.join(path, 'cache.csv'), 'a') as f:
+            f.write(f'{smi},{score},{time.time() - t0}\n')
+
+    with open(os.path.join(cwd, 'OUT_ALL.csv'), 'a') as f:
+        f.write(f'{smi},{score}\n')
+
+    os.chdir(cwd)
+    tmp_dir.cleanup()
+        
+    return score
+
+
+
+if __name__ == '__main__':
+
+    STEREO = bool(int(sys.argv[-1]))
+    print(f'Stereoisomers? : {STEREO}')
+
+    # load isoalphabet from zinc dataset
+    fname = 'data/isoalphabet.npz' if STEREO else 'data/alphabet.npz'
+    alphabet = np.load(fname, allow_pickle=True)['alphabet'].tolist()
+    print(f'Alphabet contains: {len(alphabet)}')
+    
+    alphabet = list(set(alphabet))
+    # print(f'Alphabet with required characters contains: {len(alphabet)}')
+
+    # all parameters to be set, below are defaults
+    params_dict = {
+        # Number of iterations that JANUS runs for
+        "generations": 50,
+
+        # The number of molecules for which fitness calculations are done, 
+        # exploration and exploitation each have their own population
+        "generation_size": 100,
+        
+        # Number of molecules that are exchanged between the exploration and exploitation
+        "num_exchanges": 10,
+
+        # Callable filtering function (None defaults to no filtering)
+        "custom_filter": passes_filter,
+
+        # Fragments from starting population used to extend alphabet for mutations
+        "use_fragments": True,
+
+        # An option to use a classifier as selection bias
+        "use_classifier": True,
+
+        "num_workers": 12,
+
+        # alphabet
+        "alphabet": alphabet,
+
+        "verbose_out": True,
+
+        "num_sample_frags": 100,
+
+        "exploit_num_random_samples": 400,
+        "exploit_num_mutations": 400,
+
+        "top_mols": 5,
+
+        "explr_num_random_samples": 10,
+        "explr_num_mutations": 10,
+        "crossover_num_random_samples": 5,
+
+        'stereo': STEREO,
+
+    }
+
+    # Set your SELFIES constraints (below used for manuscript)
+    default_constraints = selfies.get_semantic_constraints()
+    new_constraints = default_constraints
+    new_constraints['S'] = 2
+    new_constraints['P'] = 3
+    selfies.set_semantic_constraints(new_constraints)  # update constraints
+
+    # get initial fitnesses from csv
+    df = pd.read_csv('data/starting_smiles.csv')
+    init_fitness = df['fitness'].tolist()
+    
+    fname = 'data/starting_smiles.txt' if STEREO else 'data/starting_smiles_noniso.txt'
+    output_dir = 'RESULTS_stereo' if STEREO else 'RESULTS_nonstereo'
+    agent = JANUS(
+        work_dir=output_dir,
+        fitness_function = fitness_function,            # calculate before hand and save in zinc file
+        start_population = fname,
+        starting_fitness = init_fitness,
+        **params_dict
+    )
+
+    agent.run()
+
+    # FITNESS
+    # data = pd.read_csv('data/starting_smiles.csv')
+    # import pdb;  pdb.set_trace()
+    
+    # with open('data/starting_smiles.txt', 'w') as f:
+    #     for smi in data['smiles']:
+    #         f.write(smi+'\n')
+
+    # init_smiles = []
+    # with open('data/starting_smiles.txt', 'r') as f:
+    #     for line in f:
+    #         line = sanitize_smiles(line.strip())
+    #         if line is not None:
+    #             init_smiles.append(line)
+    # init_smiles = list(set(init_smiles))
+
+    # with multiprocessing.Pool(64) as pool:
+    #     results = pool.map(fitness_function, init_smiles)
+
+    # df = pd.DataFrame({'smiles': init_smiles, 'fitness': results})
+    # df.to_csv('starting_smiles.csv', index=False)
+
+    # TESTING ISOMERS
+    # from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers
+
+    # start_smi = Chem.MolToSmiles(Chem.MolFromSmiles('CCOC(=O)[C@@H]1CCCN(C(=O)c2nc(-c3ccc(C)cc3)n3c2CCCCC3)C1'), isomericSmiles=False)
+    # mol = Chem.MolFromSmiles(start_smi)
+    # isomers = list(EnumerateStereoisomers(mol))
+    # import pdb;  pdb.set_trace()
+    # f1 = fitness_function(Chem.MolToSmiles(isomers[0], isomericSmiles=True))
+    # f2 = fitness_function(Chem.MolToSmiles(isomers[1], isomericSmiles=True))
+
+    # print(f1)
+    # print(f2)
+
+    # img = Draw.MolsToGridImage(isomers[0:2], legends=(str(f1), str(f2)))
+    # img.save('isomers.png')
+
+    # f = fitness_function('CCOC(=O)[C@@H]1CCCN(C(=O)c2nc(-c3ccc(C)cc3)n3c2CCCCC3)C1')
+    # print(f)
+    # import pdb; pdb.set_trace()
+
+    # run code
+    # zinc_df = pd.read_csv('zinc.csv')
+
+    # smi_list = zinc_df['isosmiles'].tolist()
+    # import pdb; pdb.set_trace()
+    # print('running...')
+    # score = fitness_function(smi_list[0])
+
+    # import pdb; pdb.set_trace()
+    
+
+    # with multiprocessing.Pool(64) as pool:
+    #     results = pool.map(fitness_function, smi_list)
+
+    # np.savez('scores.npz', scores=results)
+
+    # zinc_df['docking_score'] = results
+    # zinc_df.to_csv('zinc_scores.csv', index=False)
+
+    
+    # some preprocessing
+    # zinc_df['smiles'] = zinc_df['smiles'].apply(sanitize_smiles)
+    # zinc_df = zinc_df.drop_duplicates().dropna()
+            
+    # # zinc_df['smiles'] = zinc_df['smiles'].apply()
+    # zinc_df['selfies'] = zinc_df['smiles'].apply(sf.encoder)
+
+    # isosmiles = []
+    # is_iso = []
+    # for i, r in zinc_df.iterrows():
+    #     print(i)
+    #     smi, iso = assign_stereo(r.smiles)
+    #     isosmiles.append(smi)
+    #     is_iso.append(iso)
+
+    # zinc_df['isosmiles'] = isosmiles
+    # zinc_df['is_iso'] = is_iso
+
+    # zinc_df['isoselfies'] = zinc_df.isosmiles.apply(sf.encoder)
+    # alphabet = sf.get_alphabet_from_selfies(zinc_df.isoselfies)
+
+
+    # import pdb; pdb.set_trace()
+
+    
+
+
+
---
+++ stereogeneration/mutate.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 31 12:15:57 2021
+
+@author: akshat
+"""
+from typing import Dict
+import random
+import multiprocessing
+
+import rdkit
+from rdkit import Chem
+
+import selfies 
+from selfies import encoder, decoder
+
+from .utils import get_selfies_chars
+
+def mutate_sf(sf_chars, alphabet, num_sample_frags, base_alphabet = None):
+    """
+    Given a list of SELFIES alphabets, make random changes to the molecule using 
+    alphabet. Opertations to molecules are character replacements, additions and deletions. 
+
+    Parameters
+    ----------
+    sf_chars : (list of string alphabets)
+        List of string alphabets for a SELFIE string.
+    alphabet : (list of SELFIE strings)
+        New SELFIES characters are added here and sampled.
+    num_sample_frags: (int)
+        Number of randomly sampled SELFIE strings.
+    base_alphabet: (list of SELFIE strings)
+        Main alphabet that will be appended with the introduced characters above.
+        If none, use the semantic robust alphabet.
+
+    Returns
+    -------
+    Mutated SELFIE string.
+
+    """
+    if base_alphabet is None:
+        base_alphabet = list(selfies.get_semantic_robust_alphabet())
+    random_char_idx = random.choice(range(len(sf_chars)))
+    choices_ls = [1, 2, 3]  # 1 = replacement; 2 = addition; 3=delete
+    mutn_choice = choices_ls[
+        random.choice(range(len(choices_ls)))
+    ]  # Which mutation to do:
+
+    if alphabet != []:
+        alphabet = random.sample(alphabet, num_sample_frags) + base_alphabet
+    else:
+        alphabet = base_alphabet
+
+    # Mutate character:
+    if mutn_choice == 1:
+        random_char = alphabet[random.choice(range(len(alphabet)))]
+        change_sf = (
+            sf_chars[0:random_char_idx]
+            + [random_char]
+            + sf_chars[random_char_idx + 1 :]
+        )
+
+    # add character:
+    elif mutn_choice == 2:
+        random_char = alphabet[random.choice(range(len(alphabet)))]
+        change_sf = (
+            sf_chars[0:random_char_idx] + [random_char] + sf_chars[random_char_idx:]
+        )
+
+    # delete character:
+    elif mutn_choice == 3:
+        if len(sf_chars) != 1:
+            change_sf = sf_chars[0:random_char_idx] + sf_chars[random_char_idx + 1 :]
+        else:
+            change_sf = sf_chars
+
+    return "".join(x for x in change_sf)
+
+
+def mutate_smiles(
+    smile, alphabet, num_random_samples, num_mutations, num_sample_frags, base_alphabet = None, stereo=True
+):
+    """
+    Given an input smile, perform mutations to the strucutre using provided SELFIE
+    alphabet list. 'num_random_samples' number of different SMILES orientations are 
+    considered & total 'num_mutations' are performed. 
+
+    Parameters
+    ----------
+    smile : (str)
+        Valid SMILES string.
+    alphabet : (list of str)
+        list of SELFIE strings.
+    num_random_samples : (int)
+        Number of different SMILES orientations to be formed for the input smile.
+    num_mutations : TYPE
+        Number of mutations to perform on each of different orientations SMILES.
+    num_sample_frags: (int)
+        Number of randomly sampled SELFIE strings.
+
+    Returns
+    -------
+    mutated_smiles_canon : (list of strings)
+        List of unique molecules produced from mutations.
+    """
+    mol = Chem.MolFromSmiles(smile)
+    Chem.Kekulize(mol)
+
+    # Obtain randomized orderings of the SMILES:
+    mutated_smiles_canon = []
+    for _ in range(num_random_samples):
+        # randomize
+        random_smile = rdkit.Chem.MolToSmiles(
+            mol,
+            canonical=False,
+            doRandom=True,
+            isomericSmiles=stereo,
+            kekuleSmiles=True,
+        )
+
+        # encode and turn into list of characters
+        selfies_ls = encoder(random_smile)
+        selfies_ls_chars = get_selfies_chars(selfies_ls)
+
+        # perform mutations on selfies
+        mutated_sf = []
+        for i in range(num_mutations):
+            if i == 0:
+                mutated_sf.append(mutate_sf(selfies_ls_chars, alphabet, num_sample_frags, base_alphabet))
+            else:
+                mutated_sf.append(
+                    mutate_sf(
+                        get_selfies_chars(mutated_sf[-1]), alphabet, num_sample_frags, base_alphabet
+                    )
+                )
+        
+        # canonicalize
+        for sf in mutated_sf:
+            mutated_smiles = decoder(sf)
+            try: 
+                smi_canon = Chem.MolToSmiles(
+                    Chem.MolFromSmiles(mutated_smiles, sanitize=True),
+                    isomericSmiles=stereo,
+                    canonical=True,
+                )
+                if smi_canon != "": 
+                    mutated_smiles_canon.append(smi_canon)
+            except:
+                continue
+
+    return list(set(mutated_smiles_canon))
+
+
+
+if __name__ == "__main__":
+    molecules_here = [
+        "CCC",
+        "CCCC",
+        "CCCCC",
+        "CCCCCCCC",
+        "CS",
+        "CSSS",
+        "CSSSSS",
+        "CF",
+        "CI",
+        "CBr",
+        "CSSSSSSSSSSSS",
+        "CSSSSSSSSSC",
+        "CSSSSCCSSSC",
+        "CSSSSSSSSSF",
+        "SSSSSC",
+    ]
+    A = get_mutated_smiles(
+        molecules_here, alphabet=["[C]"] * 500, num_sample_frags=200, space="Explore"
+    )
+
